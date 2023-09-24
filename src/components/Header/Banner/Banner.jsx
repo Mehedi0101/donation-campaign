55b115cd3eae41a696ab29726908eee8b1dc0e67
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-24 23:14:43 +0600

    fake data added

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
             <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center mb-5 lg:mb-10">I Grow By Helping People In Need</h1>
             <div className="flex">
                 <input className="text-sm p-3 border border-r-0 border-dark2 outline-none rounded-s-lg w-full" type="text" placeholder="Search here...." />
-                <button className="bg-primary1 px-5 font-semibold text-white h-full rounded-e-lg">Search</button>
+                <button className="bg-primary1 px-5 font-semibold text-white h-full rounded-e-lg active:text-sm transition-all">Search</button>
             </div>
         </div>
     );
